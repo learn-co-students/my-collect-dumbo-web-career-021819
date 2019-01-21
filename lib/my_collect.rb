92ccc111619bbb102018c60a8debb0cf1887d862
Jo K <joel.kohn42@gmail.com>
2019-01-21 08:55:06 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
+def my_collect(arr)
+  newarr = []
+  x = 0 
+  while x < arr.length 
+   newarr[x] = yield arr[x]
+       x += 1 
+  end 
+  return newarr
+end 
 
+languages = Proc.new {|x| x.upcase}
 
+students = Proc.new {|x| x.split(" ").first}
