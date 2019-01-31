3b8ac40ad95c6d6ec8fbfa6fbd13027acb457d27
1-8192 <allegranzia@gmail.com>
2019-01-31 14:00:42 -0500

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,13 @@
-
-
+def my_collect(array)
+  if block_given?
+    collect_array = []
+    i = 0
+  while i < array.length
+    collect_array << yield(array[i])
+    i = i + 1
+  end
+  collect_array
+else
+  "Hey! No block was given!"
+end
+end
