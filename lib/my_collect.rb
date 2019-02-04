f215e79e78a6b24383f51cb83d4e8f40ea1c40ab
Jing Cao <jingcao.yale@gmail.com>
2019-02-04 17:05:13 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+def my_collect(array)
+  i = 0
+  new_array = []
+  while i < array.length
+    new_array.push(yield array[i])
+    i = i+1
+  end
+  return new_array
+end
 
-
+array = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
